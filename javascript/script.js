cb43878314bd4942ddcc9bca1ebbf6a13ab73992
Ryan De Souza Tomaz <ryantomaz1090@gmail.com>
2022-04-06 17:00:45 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,18 @@
-function funcao1()
-{
-alert("Sua conta foi criada com sucesso!!");
+function funcao1() {
+    alert("Sua conta foi criada com sucesso!!");
 }
 
 //ANIMAÇÕES
-    
+
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = "animate";
 
 function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element){
-        if((windowTop) > element.offsetTop){
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
             element.classList.add(animationClass)
-        } else{
+        } else {
             element.classList.remove(animationClass)
         }
     })
@@ -21,36 +20,36 @@ function animeScroll() {
 
 animeScroll();
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     animeScroll();
 });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show"); 
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
 
     //SLIDE UP
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
     });
 });
 
 // Exemplo de JavaScript inicial para desativar envios de formulário, se houver campos inválidos.
 
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
 
         // Pega todos os formulários que nós queremos aplicar estilos de validação Bootstrap personalizados.
 
@@ -58,14 +57,14 @@ $(document).ready(function(){
 
         // Faz um loop neles e evita o envio
 
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-}, false);
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 })();
